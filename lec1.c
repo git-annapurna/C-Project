b44fe0d1c8182a4c4bfe274655d45c1d149a45bb
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-22 21:37:30 +0530

    Basics of C

--- lec1.c
+++ lec1.c
@@ -1,8 +1,14 @@
-#include<stdio.h>
+#include <stdio.h> //Header file  
 int main()
-{
-    int arr[4]={1,2,3,4};
-    int i;
-    printf("%d",arr[2]);
+ {
+    //Creation of variable
+    int age = 16;          // integer variable 
+    float b = 1.67;       // decimal value 
+    char grade = 'a';      // single character
 
-}
+    // Printing
+    printf("hello!\n");     //printing simple line
+    printf("Age = %d\n", age);       // %d integer ke liye
+    printf("Pi = %f\n", b);         // %f float ke liye
+    printf("Grade = %c\n", grade);   // %c character ke liye
+ }
