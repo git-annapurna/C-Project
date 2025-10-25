02c32bbf35581f440049b2fc91ef2b7ed16d2609
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-25 20:30:33 +0530

    2-D array

--- lec18.c
+++ lec18.c
@@ -1,23 +1,29 @@
+//2-D array - printing diagnonal elements and sum of columns
 #include <stdio.h>
 int main(){
    
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9} };
     int arr1[9];
     
+    printf("The diagonal elements are : ");
     for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-
-   if(i==j){
-    printf("%d ",arr[i][j]);
-   }
-        }
+      for(int j=0;j<3;j++)
+      {
+         if(i==j)
+         {
+           printf("%d ",arr[i][j]);//Prints diagonal elements
+         }
+      }
     }
-    // for(int i=0;i<3;i++){
-    //      int sum=0;
 
-    //     for(int j=0;j<3;j++){
-    //         sum=sum+arr[j][i];
-    //     }
-    //     printf("%d ",sum);
-    // }
+    printf("\nSum of columns : ");
+    for(int i=0;i<3;i++)
+    {
+        int sum=0;
+        for(int j=0;j<3;j++)
+        {
+            sum=sum+arr[j][i]; //Prints sum of columns
+        }
+        printf("%d ",sum);
+    }
 }
