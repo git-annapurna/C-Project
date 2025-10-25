5ec782e76f7f7a4c0a4e488eed14f774713ea993
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-25 20:23:51 +0530

    Pattern

--- lec8.c
+++ lec8.c
@@ -1,60 +1,61 @@
+//Printing pattern - Upper right triangle
 #include <stdio.h>
-
 int main()
 {
-    // for(int i=0;i<3;i++){
-    //     for(int j=0;j<=i;j++){
-    //         printf(" ");
-    //     }
-    //     for(int  k=3;k>i;k--){
-    //         printf("*");
-    //     }
-
-    //     printf("\n");
-    // }
-
-    // for(int i=0;i<2;i++){
-    //     // printf("%d",i);
-    //     for(int j=0;j<2;j++){
-    //         printf("%d",j);
-    //     }
-    // }
-
-    // int num = 65;
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     for (int j = 0; j <=i; j++)
-    //     {
-    //         printf(" ");
-    //     }
-    //     for(int k=3;k>i;k--){
-    //         printf("🙁");
-    //     }
-      
-    //    for(int p=3;p>=i;p--){
-    //         printf("🙁");
-    //     }
-     
-    //     // printf("\n*");
-    //     // for (int j = 4; j >i; j--)
-    //     // {
-        //     printf(" ");
-        // }
-        // for (int k = 0; k <= i; k++)
-        // {
-        //     printf("*");
-        // }
-        //   for (int p = 1; p <= i; p++)
-        // {
-        //     printf("*");
-        // }
-        // for (int p = 4; p > i; p--)
-        // {
-        //     printf("*");
-        // }
-        // for(int k =3;k>i;k--){
-        //     printf("*");
-        // }
-        // printf("\n");
-        
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf(" ");
+        }
+        for(int  k=3;k>i;k--)
+        {
+            printf("*");
+        }
+        printf("\n");
     }
+}
+
+
+
+//Printing pattern 
+#include<stdio.h>
+int main()
+{
+    int num = 65;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j <=i; j++)
+        {
+            printf(" ");
+        }
+        for(int k=3;k>i;k--){
+            printf("🙁");
+        }
+      
+       for(int p=3;p>=i;p--){
+            printf("🙁");
+        }
+        printf("\n*");
+        for (int j = 4; j >i; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("*");
+        }
+          for (int p = 1; p <= i; p++)
+        {
+            printf("*");
+        }
+        for (int p = 4; p > i; p--)
+        {
+            printf("*");
+        }
+        for(int k =3;k>i;k--){
+            printf("*");
+        }
+        printf("\n");   
+    }
+}
