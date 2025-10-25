524940ac55e3ff441a451d54ac3113f7f56887d2
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-25 20:32:20 +0530

    String

--- lec22.c
+++ lec22.c
@@ -1,53 +1,11 @@
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-
-//     char str[]="abc";
-//   int ln=    strlen(str);
-//     int freq[256]={0};
-//     for(int i=0;i<ln;i++){
-//         freq[str[i]]++;
-//     }
-//     for(int i =0;i<ln;i++){
-//         if(freq[str[i]]==1){
-//             printf("%c",str[i]);
-//             break;
-//         }
-//     }
-
-
-
-
-//     // char str[] ="hello";
-//     // char target='e';
-//     // char nStr[100];
-//     // int j=0;
-//     //  int ln=     strlen(str);
-//     //  for(int i=0;i<ln;i++){
-//     //     if(str[i]!=target){
-//     //         nStr[j]=str[i];
-//     //         j++;
-
-//     //     }
-
-//     //  }
-//     //  nStr[j]='\0';
-//     //  printf("%s",nStr);
-
-     
-
-
-// }
-
-
-// // abcbjfbebf
-
+//Sorting a string(Placing characters alphabetically)
+//Check if substring exists 
+//Finding the most frequent character
 #include <stdio.h>
 #include <string.h>
 int main(){
     char str[]="bca";
-        int ln=   strlen(str);
-    
+        int ln=strlen(str);    
     for(int i=0;i<ln-1;i++){
         for(int j=i+1;j<ln;j++){
             if(str[i]>str[j]){
@@ -57,35 +15,27 @@ int main(){
             }
         }
     }
-    printf("%s",str);
-    // char subStr[]="abc";
-    // if(strstr(str,subStr)!=NULL){
-    //     printf("true");
-    // }else{
-    //     printf("false");
-    // }
+    printf("Sorted string : %s\n",str);
 
-// target="abc"
+    char subStr[]="abc";
+    if(strstr(str,subStr)!=NULL){
+        printf("Substring exists !\n");
+    }else{
+        printf("Substring doesn't exist !\n");
+    }
 
-
-
-
-
-//     int ln=   strlen(str);
-//    int    freq[256]={0};
-//    char result;
-//    int max=0;
-//    for(int i=0;i<ln;i++){
-//     freq[str[i]]++;
-//    }
-//    for(int i=0;i<ln;i++){
-//     if(freq[str[i]]>max){
-//         max=freq[str[i]];
-//         result=str[i];
-
-//     }
-//    }
-//    printf("%c -> %d",result,max);
-
-
+    char target[]="abc";
+    int freq[256]={0};
+    char result;
+    int max=0;
+    for(int i=0;i<ln;i++){
+    freq[str[i]]++;
+     }
+    for(int i=0;i<ln;i++){
+    if(freq[str[i]]>max){
+        max=freq[str[i]];
+        result=str[i];
+       }
+     }
+   printf("Most frequent character is %c which occurs -> %d times",result,max);
 }
