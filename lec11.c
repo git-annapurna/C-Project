96a7475d1538be3db2301b47d4a9cd125ec1fb68
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-25 20:25:45 +0530

    User-Defined Fn

--- lec11.c
+++ lec11.c
@@ -1,137 +1,73 @@
-// #include <stdio.h>
-// void odd_even( ){
-// //    if(a%2==0){
-// //     printf("even");
-// //    }else{
-// //     printf("oddd");
-// //    }
-// printf("hello");
-// return 10
+//Check if number is even or odd using UDfn
+//Sum of two numbers using UDfn
+#include <stdio.h>
+void odd_even(int a){
+   if(a%2==0){
+    printf("Even\n");
+   }else{
+    printf("Odd\n");
+   }
+}
 
-// }
-// // int sum(){
-// //     int a=5;
-// //     int b=5;
-// //     printf("%d",a+b);
-// // }
-// int main(){
-//     //  int a;
-//     //  scanf("%d",&a);
-//     // odd_even(a);
-//     // odd_even(a);
-//     // odd_even();
-//     odd_even();
+int sum(){
+    int a=5;
+    int b=5;
+    printf("Sum of a and b = %d\n",a+b);
+}
+
+int main(){
+     int a;
+     printf("Enter a number : ");
+     scanf("%d",&a);
+     odd_even(a); //Call function
+     sum(); //Call sum function  
    
-//     return 0;
-//     //  printf("hello");
-
-
-// }
-
-
-    // /or(int i=0;i<=4;i++){
-    //     for(int j=0;j<=4;j++){
-    //         if(i==j|| j==(4-i)){
-    //             printf("*");
-    //         }else{
-    //             printf(" ");
-    //         }
-    //     }printf("\n");
-    // }
-
-// #include <stdio.h>
-// int sum(){
-//     int a=5;
-//     int b=10;
-//     int c=a+b;
-//     return c;
-//     // printf("%d",a+b);
-// }
-// int main(){
-//   int data=   sum();
-//   printf("%d",data);
-
-// }
-
-
-// //1234  => 1+2+3+4=10=> 1+0=1
-
-// #include <stdio.h>
- 
-//  int sum(a){
-//     // if(a%2==0){
-//     //     printf("even");
-//     // }else{
-//     //     printf("oddd");
-//     // }
-
-    
-//     // printf("hello");
-//     // return 0;
-//     // printf("hiiii");
-//     // int a=5;
-//     // int b=10;
-//     // int c=a+b;
-//     // printf("%d",c);
-//  }
-
-// int main(){
-//       int a;
-//       scanf("%d",&a);
- 
-//     sum(a);
+    return 0;
+}
 
 
 
-
-// }
-
-//     // for(int i
-//     // =0;i<=4;i++){
-//     //     for(int j=0;j<=4;j++){
-//     //         // printf("*");
-//     //         if(i==j || j==(4-i)){
-//     //             printf("*");
-//     //         }else{
-//     //             printf(" ");
-//     //         }
-    //     }printf("\n");
-    // }
-
-
+//Printing pattern of x
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<=4;i++){
+        for(int j=0;j<=4;j++){
+            if(i==j|| j==(4-i)){
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }printf("\n");
+    }
+}
 
 
 
-
-
-// #include <stdio.h>
-// int sum(){
-    
-//     // int a=5;
-// //     int b=10;
-// //     int c=a+b;
-// //     return c;
-// // }
-// // int main(){
-// //    int  data= sum();
-// //    printf("%d",data);
-
-// }
+//Sum using UDF
+#include <stdio.h>
+int sum(){
+    int a=5;
+    int b=10;
+    int c=a+b;
+    return c;
+}
+int main(){
+   int  data= sum();
+   printf("%d",data);
+}
 
 
 
-// 1234=>  1+2+3+4=10=> 1+0=1
-// 12345
+//Printing elements of array
+#include <stdio.h>
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);  
 
+    for (int i = 0; i < n; i++) {
+         printf("%d ", arr[i]);
+    }
 
-// #include <stdio.h>
-// int main() {
-//     int arr[5] = {1, 2, 3, 4, 5};
-//     int n = sizeof(arr) / sizeof(arr[0]);  
-
-//     for (int i = 0; i < n; i++) {
-//         // printf("%d ", arr[i]);
-//     }
-
-//     return 0;
-// }
+    return 0;
+}
