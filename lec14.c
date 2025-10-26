9b6bc99c17e3338d573c9d8e1382bf9ff1cfc9ce
git-annapurna <238862235+git-annapurna@users.noreply.github.com>
2025-10-26 11:24:08 +0530

    Array

--- lec14.c
+++ lec14.c
@@ -1,128 +1,83 @@
+//Sliding window approach
 #include <stdio.h>
-#include <limits.h>
 
-int maxArraySum(int arr[] ,int n,int k){
-    int wSum=0;
-    //   int arr[7]={-1,2,4,5,4,3,7};
-    for( int i=0;i<k;i++){
-        wSum= wSum+arr[i];
+int maxArraySum(int arr[], int n, int k) {
+    int wSum = 0;
+
+    // Sum of first k elements
+    for (int i = 0; i < k; i++) {
+        wSum += arr[i];
     }
-    int maxSum=wSum;
-    for( int j=k;j<n;j++){
-        wSum=wSum+arr[j]-arr[j-k];
-        if(wSum>maxSum){
-            maxSum=wSum;
+
+    int maxSum = wSum;
+
+    // Sliding window
+    for (int j = k; j < n; j++) {
+        wSum = wSum + arr[j] - arr[j - k]; // add next element, remove first element of previous window
+        if (wSum > maxSum) {
+            maxSum = wSum;
         }
     }
+
     return maxSum;
-
-    int maxSum=INT_MIN;
-        // int arr[7]={-1,2,4,5,-4,3,-7};
-    for(int i=0;i<=n-k;i++){
-        int currSum=0;
-        for(int j=i;j<i+k;j++){
-            currSum=currSum+arr[j];
-
-        }
-        if(currSum>maxSum){
-            maxSum=currSum;
-        }
-    }
-    return maxSum;
-
-
 }
-int main(){
-    int arr[7]={-1,2,4,5,4,3,7};
-    int k=3;
-     int n=    sizeof(arr)/sizeof(arr[0]);
-  int data=     maxArraySum(arr,n,k);
-  printf("%d",data);
 
+int main() {
+    int arr[7] = {-1, 2, 4, 5, 4, 3, 7};
+    int k = 3;
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    int data = maxArraySum(arr, n, k);
+    printf("%d\n", data);
 
+    return 0;
 }
 
 
-    // int arr[7]={1,2,4,5,4,3,7};
-    // int k=3
 
+    
+//Sliding window and Brute force approach
 #include <stdio.h>
+int main() {
+    int arr[7] = {1, 2, 3, 5, 6, 5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
 
-int main(){
-    int arr[7]={1,2,3,5,6,5,4};
-    int k=3;
-    int n=     sizeof(arr)/sizeof(arr[0]);
-    int windowSum=0;
-    for(int i=0;i<k;i++){
-           int arr[7]={1,2,3,5,6,5,4};
-        windowSum=windowSum+arr[i];
+    // Sliding window approach
+    int windowSum = 0;
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
     }
-    int maxSum=windowSum;
+    int maxSum = windowSum;
 
-    for(int j=k;j<n;j++){
-        windowSum=windowSum+arr[j]-arr[j-k];
-        if(windowSum>maxSum){
-            maxSum=windowSum;
-        }
-
-    }
-    printf("%d",maxSum);
-
-    int maxSum=0;
-
-    for(int i=0;i<n-k;i++){
-        //   int arr[7]={1,2,3,5,6,5,4};
-        int currSum=0;
-        for(int j=i;j<i+k;j++){
-            currSum=currSum+arr[j];
-        }
-        if(currSum>maxSum){
-            maxSum=currSum;
+    for (int j = k; j < n; j++) {
+        windowSum = windowSum + arr[j] - arr[j - k];
+        if (windowSum > maxSum) {
+            maxSum = windowSum;
         }
     }
-    printf("%d",maxSum);
+    printf("Max sum (sliding window) = %d\n", maxSum);
 
-    // for(int i)
-    // int target=5;
-
-
-
-
-
-    for(int i=0;i<5;i++){
-        for(int j=i+1;j<5;j++){
-            if(arr[i] +arr[j]==target){
-
-            }
+    // Brute force approach
+    int maxSumBF = 0;
+    for (int i = 0; i <= n - k; i++) {
+        int currSum = 0;
+        for (int j = i; j < i + k; j++) {
+            currSum += arr[j];
+        }
+        if (currSum > maxSumBF) {
+            maxSumBF = currSum;
         }
     }
-    for(int i=0;i<5;i++){
-        if(arr[i]==target){
-            printf("%d",i);
+    printf("Max sum (brute force) = %d\n", maxSumBF);
+
+    //find a target in array
+    int target = 5;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            printf("Target %d found at index %d\n", target, i);
             break;
         }
     }
-
-
-
-
+    return 0;
 }
-
-
-{1,2,3,4,5,9}
-k=3
-
-
-
-
-{1,2,4} 1+2+4=7
-
-1+2+3+4=10-7=3
- 360 => 101=> 102
-
-
-
-{1,2,3,3,2,1}
-
-{ }
